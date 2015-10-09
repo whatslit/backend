01a6f7dbba5997b383b86ddbb7c75b1124a12a07
William Guss <carlostnb@gmail.com>
2015-10-09 01:27:37 -0700

    First working build for the team.

--- whatslit/settings.py
+++ whatslit/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'whatslitdb',
-        'USER': '',
-        # 'PASSWORD': 'admin',
+        'USER': 'litadmin',
+        'PASSWORD': 'lit',
     },
 }
 
